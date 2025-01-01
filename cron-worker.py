b7fabac64bae735c5ab0d416699793c1d055f76e
Zane George <146121353+Zane-Dev14@users.noreply.github.com>
2025-01-01 22:10:47 +0530

    Lets see

--- cron-worker.py
+++ cron-worker.py
@@ -1,10 +1,12 @@
 import time
 from db import connect_db
 from utils import fetch_filtered_prs, update_leaderboard
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 def cron_worker():
     client = connect_db()
-
+    logging.info("Cron worker started...")
     while True:
         fetch_filtered_prs(client)
         update_leaderboard(client)
